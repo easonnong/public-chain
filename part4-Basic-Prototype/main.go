1914e3d08b64c38de2f77e0de162ecf378e704d2
easonnong <nongeason@gmail.com>
2022-06-27 10:44:47 +0800

    add new block

---
+++ part4-Basic-Prototype/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/easonnong/public-chain/part4-Basic-Prototype/BLC"
+)
+
+func main() {
+	// genesis block
+	blockchain := BLC.CreateBlockchainWithGenesisBlock()
+
+	// new block
+	blockchain.AddBlockToBlockchain(
+		"send 1 bitcoin to satoshi",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash,
+	)
+	blockchain.AddBlockToBlockchain(
+		"send 2 bitcoin to satoshi",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash,
+	)
+
+	fmt.Println(blockchain)
+}
