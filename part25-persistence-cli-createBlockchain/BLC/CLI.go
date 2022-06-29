4e008c96a78100a244576f0dbb4d7a09c6c2ad04
easonnong <nongeason@gmail.com>
2022-06-29 10:00:50 +0800

    create blockchain cmd

---
+++ part25-persistence-cli-createBlockchain/BLC/CLI.go
@@ -0,0 +1,96 @@
+package BLC
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+type CLI struct {
+	BlockChain *BlockChain
+}
+
+// print how to use cli
+func (cli *CLI) printUsage() {
+	fmt.Println("\nHere is a usage...")
+	fmt.Println("\tcreateblockchain -data DATA")
+	fmt.Println("\taddBlock -data DATA")
+	fmt.Println("\tprintchain")
+}
+
+// judge args is valid
+func (cli *CLI) isValidArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
+// create genesis block
+func (cli *CLI) createGenesisBlockchain(data string) {
+	fmt.Println(data)
+}
+
+// add block into blockchain
+func (cli *CLI) addBlock(data string) {
+	cli.BlockChain.AddBlockToBlockchain(data)
+}
+
+// print all block data
+func (cli *CLI) printchain() {
+	cli.BlockChain.PrintChain()
+}
+
+// cli run
+func (cli *CLI) Run() {
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
+	flagAddBlockData := addBlockCmd.String("data", "block data...", "block data")
+	flagCreateBlockchainData := createBlockchainCmd.String("data", "genesis block data...", "genesis block data")
+
+	cli.isValidArgs()
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "createblockchain":
+		err := createBlockchainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *flagAddBlockData == "" {
+			cli.printUsage()
+			os.Exit(1)
+		}
+		cli.addBlock(*flagAddBlockData)
+	}
+
+	if createBlockchainCmd.Parsed() {
+		if *flagCreateBlockchainData == "" {
+			cli.printUsage()
+			os.Exit(1)
+		}
+		cli.createGenesisBlockchain(*flagCreateBlockchainData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printchain()
+	}
+}
---
+++ part25-persistence-cli-createBlockchain/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/easonnong/public-chain/part25-persistence-cli-createBlockchain/BLC"
+)
+
+func main() {
+
+	cli := &BLC.CLI{}
+
+	cli.Run()
+}
