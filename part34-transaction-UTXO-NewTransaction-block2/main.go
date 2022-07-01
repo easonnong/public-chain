c34aa2781f2f18a2e57ded9c63002d8aa836de9c
easonnong <nongeason@gmail.com>
2022-07-01 15:44:15 +0800

    transfer

---
+++ part34-transaction-UTXO-NewTransaction-block2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/easonnong/public-chain/part34-transaction-UTXO-NewTransaction-block2/BLC"
+)
+
+func main() {
+
+	cli := &BLC.CLI{}
+
+	cli.Run()
+}
---
+++ part35-transaction-UTXO-NewTransaction-block3/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/easonnong/public-chain/part35-transaction-UTXO-NewTransaction-block3/BLC"
+)
+
+func main() {
+
+	cli := &BLC.CLI{}
+
+	cli.Run()
+}
