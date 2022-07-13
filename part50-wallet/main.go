d577ba67f9d5d2ffc4cc6f51e9a1602e188ddf5e
easonnong <nongeason@gmail.com>
2022-07-13 15:48:35 +0800

    wallet

---
+++ part50-wallet/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/easonnong/public-chain/part50-wallet/BLC"
+)
+
+func main() {
+	bytes := []byte("https://github.com/easonnong")
+
+	bytes58 := BLC.Base58Encode(bytes)
+
+	fmt.Printf("%x\n", bytes58)
+	fmt.Printf("%s\n", bytes58)
+
+	bytes58Str := BLC.Base58Decode(bytes58)
+
+	fmt.Printf("%s\n", bytes58Str)
+	fmt.Printf("%s\n", bytes58Str[1:])
+
+}
